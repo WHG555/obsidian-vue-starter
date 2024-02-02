fa2462dcfcc8991a62f8fd3ac5428d45d09413cf
whghcyx <whghcyx@outlook.com>
2024-02-03 01:58:33 +0800

    添加备注信息

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import {
 
 import { MyView, VIEW_TYPE } from './view'
 
-
+// 接口和类
 interface MyPluginSettings {
     mySetting: string;
 }
@@ -18,10 +18,13 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
     mySetting: 'default'
 }
 
+// 插件类
 export default class MyPlugin extends Plugin {
     settings: MyPluginSettings;
 
+    // 加载时运行
     async onload() {
+        console.log("plugin loading");
         await this.loadSettings();
 
         this.registerView(
@@ -29,20 +32,25 @@ export default class MyPlugin extends Plugin {
             (leaf) => new MyView(leaf)
         )
 
+        // 这将在左侧功能区中创建一个图标。
         this.addRibbonIcon('dice', 'Open my view', (evt) => {
             this.activateView()
         })
 
     }
 
+    // 卸载时运行
     onunload() {
+        console.log("plugin unloading");
         this.app.workspace.detachLeavesOfType(VIEW_TYPE)
     }
 
+    // 导入设置
     async loadSettings() {
         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
     }
 
+    // 保存设置
     async saveSettings() {
         await this.saveData(this.settings);
     }
